77bd1cf87e00503a3833438e383406de090096e3
David <david.04.fsl@gmail.com>
2022-11-07 18:28:39 +0100

    f

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,13 +6,29 @@
 /*   By: dlima <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 14:32:13 by dlima             #+#    #+#             */
-/*   Updated: 2022/11/07 15:42:02 by dlima            ###   ########.fr       */
+/*   Updated: 2022/11/07 18:25:49 by dlima            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
+	char	*ptr;
+	size_t	size;
+	size_t	len_s1;
 
+	size = ft_strlen(s1) + ft_strlen(s2) +1;
+	ptr = (char *)malloc(sizeof(char) * size);
+	ft_strlcpy(ptr, s1, size);
+	len_s1 = ft_strlen(s1);
+	ft_strlcpy(&ptr[len_s1], s2, size - len_s1);
+	return (ptr);
 }
+
+// int	main()
+// {
+// 	char	s1[] = "d";
+// 	char	s2[] = "lima";
+// 	printf("%s", ft_strjoin(s1, s2));
+// }
--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,12 +6,25 @@
 /*   By: dlima <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/21 16:16:29 by dlima             #+#    #+#             */
-/*   Updated: 2022/10/24 16:45:37 by dlima            ###   ########.fr       */
+/*   Updated: 2022/11/07 17:49:13 by dlima            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcat(char *dst, const char *src, size_t size)
+size_t	ft_strlcat(char *dest, const char *src, size_t size)
 {
+	char	*pdest;
+	size_t	i;
+
+	if (!src)
+		return (0);
+	pdest = dest;
+	i = ft_strlen(dest) + ft_strlen(src);
+	while (*pdest != '\0')
+		pdest++;
+	if (size > 0)
+		ft_memcpy(pdest, src, size);
+	pdest[size] = '\0';
+	return (i);
 }
