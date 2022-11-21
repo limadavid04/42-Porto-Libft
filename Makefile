202584b0753752480658c73d1890b64da27702a5
David <david.04.fsl@gmail.com>
2022-11-21 22:07:44 +0100

    .

--- Makefile
+++ Makefile
@@ -10,7 +10,8 @@ ft_calloc   ft_memccpy  ft_split    ft_strlen   ft_tolower\
 ft_isalnum  ft_memchr   ft_strchr   ft_strncmp  ft_toupper\
 ft_isalpha  ft_memcmp   ft_strdup   ft_strnstr ft_itoa ft_strmapi\
 ft_striteri ft_putchar_fd ft_putstr_fd ft_putendl_fd ft_putnbr_fd
-BONUS = ft_lstnew ft_lstadd_front ft_lstsize ft_lstlast ft_lstadd_back
+BONUS = ft_lstnew ft_lstadd_front ft_lstsize ft_lstlast ft_lstadd_back\
+ft_lstdelone
 all: $(NAME)
 
 $(NAME): $(FILES:=.o)
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: dlima <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/15 15:31:02 by dlima             #+#    #+#             */
-/*   Updated: 2022/11/21 21:48:54 by dlima            ###   ########.fr       */
+/*   Updated: 2022/11/21 22:04:55 by dlima            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,4 +63,5 @@ void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void*));
 #endif
