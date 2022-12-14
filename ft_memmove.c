64005803a0e7990b2298493e3bdf57db1f6dc9c4
limadavid04 <david.04.fsl@gmail.com>
2022-12-14 16:54:55 +0000

    .

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: dlima <dlima@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/17 19:18:40 by dlima             #+#    #+#             */
-/*   Updated: 2022/12/14 16:47:04 by dlima            ###   ########.fr       */
+/*   Updated: 2022/12/14 16:53:36 by dlima            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,11 @@
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	size_t		i;
+	char		*ptr_dest;
+	char		*ptr_src;
 
+	ptr_src = (char *)src;
+	ptr_dest = dest;
 	i = 0;
 	if (!src && !dest)
 		return (NULL);
